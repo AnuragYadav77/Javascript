f6b72e7086023eabfe5394031b774b13d3ab1dbc
AnuragYadav77 <anuragyadav8871@gmail.com>
2025-11-07 04:16:27 +0000

    Array methods covered.

--- 02_basics/1_arrays.js
+++ 02_basics/1_arrays.js
@@ -60,10 +60,10 @@ person3["age"] = 46;
 
 ///A Common Error:
 const elements=[40]; //creates an array with one element
-console.log(elements);
+// console.log(elements);
 ///BUT
 const elem=new Array(40); //creates an array with 40 undefined elements
-console.log(elem);
+// console.log(elem);
 
 
 
@@ -80,4 +80,101 @@ anotherArray.push(6);
 anotherArray.pop(); //no argument shall be passed
 // console.log(anotherArray);
 
+//shift() - removes the 'first' array element and "shifts" all other elements to a lower index
+const shiftArray=[1,2,3,4,5];
+// console.log(shiftArray.shift()); //1
+// console.log(shiftArray); // [2,3,4,5]
 
+
+//unshift()-The unshift() method adds a new element to an array (at the beginning), and "unshifts" older elements:
+const unshiftArray=[2,3,4,5];
+unshiftArray.unshift(1);
+// console.log(unshiftArray); // [1,2,3,4,5]
+
+
+//length()- returns the length of the array.It can also be used to set the length of the array.
+const lenArray=[1,2,3,4,5,6,7,8,9]; //original length=9
+lenArray.length=5; //sets the length to 5
+// console.log(lenArray); 
+
+//NOTE:The length property provides an easy way to append a new element to an array:
+const fruits2 = ["Banana", "Orange", "Apple", "Mango"];
+fruits2[fruits2.length] = "Kiwi";
+// console.log(fruits2); //["Banana", "Orange", "Apple", "Mango", "Kiwi"]
+
+
+
+
+//toString() - returns the elements of an array as a comma separated string
+const strArray=[10,20,30,40,50];
+const str=strArray.toString();
+// console.log(str); // 10,20,30,40,50
+
+//at() - returns the element at the specified index
+// console.log(strArray.at(4));
+
+//WARNING!! : Negative indexing is not allowed in JS to access an element from the end of an array, because [] is used for accessing both arrays and objects.
+
+
+// join() - joins all elements of an array into a string.It behaves just like toString(), but in addition you can specify the separator:
+
+const joinArray=["Hello","World","from","JS"];const joined=joinArray.join("--");
+// console.log(joined);
+
+
+// concat() - The concat() method creates a 'new array' by merging (concatenating) existing arrays. it can take any number of array arguments:
+const array1 = ["Cecilie", "Lone"];
+const array2 = ["Emil", "Tobias", "Linus"];
+const array3 = ["Robin"];
+// console.log(array1.concat(array2,array3));
+//it also takes strings as arguments.
+// console.log(array2.concat("peter"))
+
+
+//copyWithin() -  The copyWithin() method copies array elements to another position in the array, overwriting the existing values.It does not add items to the array.
+const copyArray=["A","B","C","D","E","F","G"];
+// console.log(copyArray.copyWithin(2,0)); //copy to index 2, all elements from index 0
+//the remaining elements are shifted accordingly but the length remains same.
+//output: ["A","B","A","B","C","D","E"]
+//The copyWithin() method overwrites the existing values.
+
+
+
+//Flattening an Array:
+//Flattening an array is the process of reducing the dimensionality of an array.Flattening is useful when you want to convert a multi-dimensional array into a one-dimensional array.
+
+//flat(depth)-creates a new array with sub-array elements concatenated to a specified depth.default depth is 1.
+const flatArray=[1,2,[3,4],[5,6]];
+const flatArray2=[[1,2],[3,4],[5,[6,7]]];
+// console.log(flatArray2.flat(2)); //here depth is 2
+//The depth level specifying how deep a nested array structure should be flattened
+
+
+//flatMap() - first maps each element using a mapping function, then flattens the result into a new array. It is identical to a map followed by a flat of depth 1.
+//syntax: array.flatMap(function(currentValue, index, arr), thisValue)
+const flatMapArray=[1,2,3,4];
+// console.log(flatMapArray.flatMap()); // no mapping function provided here
+//output: [1,2,3,4]
+//In this example, since no mapping function is provided, flatMap() simply returns a shallow copy of the original array.
+// console.log(flatMapArray.flatMap(x => [x, x * 2]));
+
+
+//SPLICING AND SLICING ARRAYS
+
+//splice() - The splice() method adds/removes items to/from an array, and returns an array with the deleted/removed item(s).
+const spliceArray=["Jan","March","April","June"];
+//  console.log(spliceArray.splice(1,0,"Feb")); //inserts at index 1, 0 elements removed.IT RETURNS AN EMPTY ARRAY AS NO ELEMENTS ARE REMOVED.
+//The first parameter (1) defines the position where new elements should be added (spliced in).The second parameter (0) defines how many elements should be removed.The rest of the parameters define the new elements to be added.
+// console.log(spliceArray); //["Jan", "Feb", "March", "April", "June"] 
+
+
+
+//toSpliced()-The difference between the new toSpliced() method and the old splice() method is that the new method creates a new array, keeping the original array unchanged, while the old method altered the original array.
+
+
+
+//slice()- The slice() method returns a 'shallow copy' of a portion of an array into a new array object selected from start to end (end not included).The original array will not be modified.
+const sliceArray=["Jan","Feb","March","April","May","June"];
+const newSliceArray=sliceArray.slice(1,4); //from index 1 to index 4(not included)
+console.log(newSliceArray); //["Feb", "March", "April"]
+console.log(sliceArray); //original array remains unchanged
